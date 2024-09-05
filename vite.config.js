4f195e070f62f1804181590620dfaee5c3434c09
Oblivion <d28112002322@gmail.com>
2024-09-05 03:56:48 +0300

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-deploy/", // YOUR REPO NAME HERE
-});
+  base: '/Task-mangment/' // Add this line
+})
